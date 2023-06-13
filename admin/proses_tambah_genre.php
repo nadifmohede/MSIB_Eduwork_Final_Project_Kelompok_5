a851660cbb2fc57ba908c6fd4387c12510a0d4e8
Syahrul yuda <90370914+syahrulyudaa@users.noreply.github.com>
2023-06-13 08:36:58 +0700

    Delete admin directory

--- admin/proses_tambah_genre.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $judul_genre = $_POST['judul_genre'];
-
-    include('../database/koneksi.php');
-
-    $result = mysqli_query($koneksi, "INSERT INTO `genre` (`judul_genre`) VALUES ('$judul_genre'); ");
-
-    header("Location: data_genre.php");
-?>
--- admin/proses_tambah_sutradara.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $nama_sutradara = $_POST['nama_sutradara'];
-
-    include('../database/koneksi.php');
-
-    $result = mysqli_query($koneksi, "INSERT INTO `sutradara` (`nama_sutradara`) VALUES ('$nama_sutradara'); ");
-
-    header("Location: data_sutradara.php");
-?>
