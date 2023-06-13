7831d38ca90e26badb85f3d692dbd447881916bc
Syahrul yuda <90370914+syahrulyudaa@users.noreply.github.com>
2023-06-13 08:37:43 +0700

    Add files via upload

---
+++ proses_edit/proses_edit_pemeran.php
@@ -0,0 +1,12 @@
+<?php
+    $id_pemeran = $_POST['id_pemeran'];
+    $id_aktor = $_POST['id_aktor'];
+    $id_film = $_POST['id_film'];
+    $peran = $_POST['peran'];
+
+    include('../database/koneksi.php');
+
+    $result = mysqli_query($koneksi, "UPDATE pemeran SET id_aktor = '$id_aktor', id_film = '$id_film', peran = '$peran',  WHERE id_pemeran = '$id_pemeran' ");
+
+    header("Location: data_pemeran.php");
+?>
